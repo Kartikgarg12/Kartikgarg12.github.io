6cd63cfb4bdd2bb02de48130f3c6c727478f15ec
Kartik Garg <kartikgarg@Kartiks-MacBook-Pro.local>
2022-03-14 09:29:31 +0530

    pushing code for react chat app deployment

---
+++ src/components/chatContent/ChatContent.js
@@ -0,0 +1,128 @@
+import React, { Component, useState, createRef, useEffect } from "react";
+
+import "./chatContent.css";
+
+import ChatItem from "./ChatItem";
+
+
+export default class ChatContent extends Component {
+  messagesEndRef = createRef(null);
+  chatItms = [
+    {
+      key: 1,
+      type: "",
+      msg: "Hi inFeedo, this is KARTIK GARG",
+    },
+    {
+      key: 2,
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTA78Na63ws7B7EAWYgTr9BxhX_Z8oLa1nvOA&usqp=CAU",
+      type: "other",
+      msg: "Hi Kartik",
+    },
+    {
+      key: 3,
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTA78Na63ws7B7EAWYgTr9BxhX_Z8oLa1nvOA&usqp=CAU",
+      type: "",
+      msg: "This is my Chat Page assignment",
+    },
+    {
+      key: 4,
+      image:
+        "https://pbs.twimg.com/profile_images/1116431270697766912/-NfnQHvh_400x400.jpg",
+      type: "other",
+      msg: "Wow",
+    }
+  ];
+
+  
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      chat: this.chatItms,
+      msg: ""
+    };
+  }
+  scrollToBottom = () => {
+    this.messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+
+  componentDidMount() {
+    window.addEventListener("keydown", (e) => {
+      if (e.keyCode == 13) {
+        if (this.state.msg != "") {
+          this.chatItms.push({
+            key: 1,
+            type: "",
+            msg: this.state.msg,
+            image:""
+          });
+          this.chatItms.push({
+            key: 1,
+            type: "other",
+            msg: this.state.msg,
+            image:""
+          });
+          this.setState({ chat: [...this.chatItms] });
+          this.scrollToBottom();
+          this.setState({ msg: "" });
+        }
+      }
+    });
+    this.scrollToBottom();
+  }
+  onStateChange = (e) => {
+    this.setState({ msg: e.target.value });
+    const msg = e.target.value;
+    localStorage.setItem("msg",msg);
+    this.setState({msg});
+  };
+  
+  render() {
+    return (
+      <div className="main__chatcontent">
+        <div className="content__header">
+          <div className="blocks">
+            <div className="current-chatting-user">
+
+              <p>inFeedo</p>
+            </div>
+          </div>
+
+
+        </div>
+        <div className="content__body">
+          <div className="chat__items">
+            {this.state.chat.map((itm, index) => {
+              return (
+                <ChatItem
+                  animationDelay={index + 2}
+                  key={itm.key}
+                  user={itm.type ? itm.type : "me"}
+                  msg={itm.msg}
+                  image={itm.image}
+                />
+              );
+            })}
+            <div ref={this.messagesEndRef} />
+          </div>
+        </div>
+        <div className="content__footer">
+          <div className="sendNewMessage">
+            
+            <input
+              type="text"
+              placeholder="Type a message here and press enter"
+              onChange={this.onStateChange}
+              value={this.state.msg}
+            />
+            
+
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
